29ecdd5664bed4412ea3b4476116285e7dc3c850
yosuke7040 <index_railgu@yahoo.co.jp>
2023-09-18 21:31:17 +0900

    ch04

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # go-grpc
+
+```bash
+cd api
+protoc --go_out=../pkg/grpc --go_opt=paths=source_relative \
+       --go-grpc_out=../pkg/grpc --go-grpc_opt=paths=source_relative \
+       hello.proto
+```
---
+++ cmd/server/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	port := 8080
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+
+	go func() {
+		log.Printf("start gRPC server port: %v", port)
+		s.Serve(listener)
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("stopping gRPC server...")
+	s.GracefulStop()
+}
