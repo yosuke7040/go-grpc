9188523e37f3ca78e8bc362104ccdf094ed1f637
yosuke7040 <index_railgu@yahoo.co.jp>
2023-09-19 23:40:30 +0900

    ch06

---
+++ cmd/client/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	hellopb "github.com/yosuke7040/go-grpc/pkg/grpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	scanner *bufio.Scanner
+	client  hellopb.GreetingServiceClient
+)
+
+func main() {
+	fmt.Println("start gRPC client...")
+
+	scanner = bufio.NewScanner(os.Stdin)
+
+	address := "localhost:8080"
+	conn, err := grpc.Dial(
+		address,
+
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		log.Fatal("Connection failed: ", err)
+		return
+	}
+	defer conn.Close()
+
+	client = hellopb.NewGreetingServiceClient(conn)
+
+	for {
+		fmt.Println("1: send Request")
+		fmt.Println("2: exit")
+		fmt.Print("please enter >")
+
+		scanner.Scan()
+		in := scanner.Text()
+
+		switch in {
+		case "1":
+			Hello()
+		case "2":
+			fmt.Println("bye.")
+			goto M
+		}
+	}
+M:
+}
+
+func Hello() {
+	fmt.Println("Please enter your name.")
+	scanner.Scan()
+	name := scanner.Text()
+
+	req := &hellopb.HelloRequest{
+		Name: name,
+	}
+	res, err := client.Hello(context.Background(), req)
+	if err != nil {
+		fmt.Println("error: ", err)
+	} else {
+		fmt.Println(res.GetMessage())
+	}
+}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"os"
 	"os/signal"
 
+	hellopb "github.com/yosuke7040/go-grpc/pkg/grpc"
+
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
+
+type myServer struct {
+	hellopb.UnimplementedGreetingServiceServer
+}
+
+func NewMyServer() *myServer {
+	return &myServer{}
+}
+
+func (s *myServer) Hello(ctx context.Context, req *hellopb.HelloRequest) (*hellopb.HelloResponse, error) {
+	return &hellopb.HelloResponse{
+		Message: fmt.Sprintf("Hello, %s", req.Name),
+	}, nil
+}
 
 func main() {
 	port := 8080
@@ -18,6 +36,10 @@ func main() {
 	}
 
 	s := grpc.NewServer()
+
+	hellopb.RegisterGreetingServiceServer(s, NewMyServer())
+
+	reflection.Register(s)
 
 	go func() {
 		log.Printf("start gRPC server port: %v", port)
